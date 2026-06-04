1e922aa4947e9c88996666e08a525780448db3f6
23f2004256 <23f2004256@ds.study.iitm.ac.in>
2026-06-04 11:18:18 +0530

    refactor: format Flask app initialization for improved readability

--- api/index.py
+++ api/index.py
@@ -16,7 +16,10 @@ from routes.auth import auth_bp
 from routes.dashboard import dashboard_bp
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(
+    __name__,
+    template_folder="../templates",
+    static_folder="../static")
     app.config.from_object(config_class)
 
     # Ensure database and upload directories exist
